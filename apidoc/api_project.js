118f25d111c23c603ab75046a63f6c5addf580b3
karin.duss <karin.duss@students.fhnw.ch>
2020-08-28 22:16:47 +0200

    Add api doc

---
+++ apidoc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "",
+  "version": "0.0.0",
+  "description": "",
+  "apidoc": "0.3.0",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-07-08T10:14:47.034Z",
+    "url": "http://apidocjs.com",
+    "version": "0.23.0"
+  }
+});
